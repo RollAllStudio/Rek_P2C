3d39f9a18d3b9c4056a5d7f365ec425c2951d0a4
RollAllStudio <mikolajjaworski1996@gmail.com>
2025-09-21 11:57:27 +0200

    match finished widget

--- Source/TPPMulti/UI/HUD/Private/MatchHUDWidget.cpp
+++ Source/TPPMulti/UI/HUD/Private/MatchHUDWidget.cpp
@@ -2,3 +2,28 @@
 
 
 #include "TPPMulti/UI/HUD/Public/MatchHUDWidget.h"
+
+#include "MultiplayerGameSubsystem.h"
+#include "Components/TextBlock.h"
+#include "Components/WidgetSwitcher.h"
+#include "MultiplayerGame/Server/Public/ServerPlayerState.h"
+#include "TPPMulti/Core/MatchPlayer/Public/MatchPlayerController.h"
+
+void UMatchHUDWidget::OnPlayerWin(const int32& InPlayerUID)
+{
+
+	AServerPlayerState* WinnerPlayerState =
+		UMultiplayerGameSubsystem::GetServerPlayerState(this, InPlayerUID);
+	WinnerNameBox->SetText(FText::FromString(WinnerPlayerState->GetServerPlayerName()));
+	MainSwitcher->SetActiveWidgetIndex(1);
+}
+
+void UMatchHUDWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	MainSwitcher->SetActiveWidgetIndex(0);
+	AMatchPlayerController* MatchPlayerController =
+		Cast<AMatchPlayerController>(GetOwningPlayer());
+
+	MatchPlayerController->OnPlayerWin.AddUniqueDynamic(this, &UMatchHUDWidget::OnPlayerWin);
+}
--- Source/TPPMulti/UI/HUD/Public/MatchHUDWidget.h
+++ Source/TPPMulti/UI/HUD/Public/MatchHUDWidget.h
@@ -6,6 +6,9 @@
 #include "Blueprint/UserWidget.h"
 #include "MatchHUDWidget.generated.h"
 
+class UTextBlock;
+class UOverlay;
+class UWidgetSwitcher;
 /**
  * 
  */
@@ -13,4 +16,56 @@ UCLASS()
 class TPPMULTI_API UMatchHUDWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+	UFUNCTION()
+	void OnPlayerWin(const int32& InPlayerUID);
+
+protected:
+
+	virtual void NativeOnInitialized() override;
+
+#pragma region BindWidgets
+
+private:
+
+	UPROPERTY(meta=(BindWidget), BlueprintGetter = GetMainSwitcher)
+	TObjectPtr<UWidgetSwitcher> MainSwitcher;
+
+	UPROPERTY(meta=(BindWidget), BlueprintGetter = GetFinishedMatchOverlay)
+	TObjectPtr<UOverlay> FinishedMatchOverlay;
+
+	UPROPERTY(meta=(BindWidget), BlueprintGetter = GetWinnerNameBox)
+	TObjectPtr<UTextBlock> WinnerNameBox;
+
+	UPROPERTY(meta=(BindWidget), BlueprintGetter = GetMatchOverlay)
+	TObjectPtr<UOverlay> MatchOverlay;
+	
+public:
+
+	UFUNCTION(BlueprintGetter)
+	UWidgetSwitcher* GetMainSwitcher() const
+	{
+		return MainSwitcher;
+	}
+
+	UFUNCTION(BlueprintGetter)
+	UOverlay* GetFinishedMatchOverlay() const
+	{
+		return FinishedMatchOverlay;
+	}
+
+	UFUNCTION(BlueprintGetter)
+	UTextBlock* GetWinnerNameBox() const
+	{
+		return WinnerNameBox;
+	}
+
+	UFUNCTION(BlueprintGetter)
+	UOverlay* GetMatchOverlay()
+	{
+		return MatchOverlay;
+	}
+	
+#pragma endregion 
+	
 };
